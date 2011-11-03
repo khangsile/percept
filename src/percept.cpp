432576eaeccd48f333df39151b45c0004173e4f3
Aniket Handa <animus.aniket@gmail.com>
2011-11-03 16:58:35 +0530

    lucasshow and lucas

--- src/percept.cpp
+++ src/percept.cpp
@@ -1,44 +1,5 @@
 #include "lucas.h"
 
-#include <cv.h>
-
-#include <highgui.h>
-
-class pcMatrixPercept
-{
-    int matrix[1920][1080];
-    CvSize f_size;
-    
-    pcMatrixPercept(CvSize frame_size)
-    {
-        f_size=frame_size;
-        for (int i=0; i<frame_size.width; i++) 
-        {
-            for (int j=0; j<frame_size.height; j++) 
-            {
-                matrix[i][j]=0;
-            }
-        }
-    }
-    int setRelDepth0to255(int x, int y, int value)
-    {
-        if(x<0 || x>f_size.width || y>f_size.height || y<0)
-        {    
-            return -1;
-        }
-        
-        if(value>256)
-            value=value%256;
-        matrix[x][y]=value;
-        return 0;
-      
-    }
-    int getRelDepth0to255(int x, int y)
-    {
-        return matrix[x][y];
-    }
-};
-
 int initPercept(int givenWidth, int givenHeight, char* source, CvCapture **input_video)
 {
     
